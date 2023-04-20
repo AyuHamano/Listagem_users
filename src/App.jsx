ba2d796c8f74a72bbf5de9ac991de9d02570ea46
Aline <hamanoaline15@gmail.com>
2023-04-20 02:14:14 -0300

    organização do layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@ import axios from 'axios'
 import { AgGridReact } from 'ag-grid-react'
 import 'ag-grid-community/styles/ag-grid.css'
 import 'ag-grid-community/styles/ag-theme-alpine.css'
-//import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
-
-
 
 function App () { 
   const [loading, setLoading] = useState(false); 
@@ -45,7 +42,6 @@ function App () {
           if (searchTitle === "" || searchType === "") {
             return value;
           }
-
           else if (value[searchType].toLowerCase().includes(searchTitle.toLowerCase())) {
             return value;
           }
@@ -57,28 +53,54 @@ function App () {
     sortable: true, //ordenar
     filter: true 
   }), []);
+
+  const tamanhoPagina = 8; //tamanho da página
   
 
   return (
-    <div className="w-3 container">
-      <form className='busca'>
-        <label id='titulo' htmlFor="search">Cadastros de usuários</label>
-        <input type="search" placeholder='Pesquisar' id='search-board' onChange={(e) => setSearchTitle(e.target.value)}/>
+    <div className="container">
 
-        <select name ='filtro' id="filtro" onChange={(e) => setSearchType(e.target.value)}>
-            
-              <option>Filtrar</option>
-              <option value="nomeCompleto">Nome Completo</option>
-              <option value="nomeSocial">Nome Social</option>
-              <option value="email">Email</option>
-
+      <nav className='navigation'>
           
-        </select>
-        {console.log(searchType)} 
-      </form>
+          <div className ="nav-logo">
+            <a href='https://www.labtime.ufg.br/site/#/'>
+              <img id='logo' src="img/marca_labtime.png" alt="logo"/>
+            </a>
+          </div>
+          <h1>Listas de Usuários</h1>
+          <ul>
+            <a href=""><li>Contato</li></a>
+            <a href=""><li>Sobre</li></a>
+            
+          </ul>
+        
+      </nav>
+      
+      <div className="busca">
+      <div className="busca-select">
+          
+          <select name ='filtro' id="busca-filtro" onChange={(e) => setSearchType(e.target.value)}>
+            <option value= "Filtrar">Filtrar</option>
+            <option value="nomeCompleto">Nome Completo</option>
+            <option value="nomeSocial">Nome Social</option>
+            <option value="email">Email</option>
+          </select>
+
+      </div>     
+
+      <div className="input">
+        <input type="search" placeholder='Pesquisar' id='busca-campo' onChange={(e) =>   setSearchTitle(e.target.value)}/>
+      </div>
+
+      
+
+      </div>
+          
+          {console.log(searchType)} 
+      
 
 
-     <div id="myGrid" className='ag-theme-alpine' style={{height: 468, width: 1365, marginTop: 147.5, fontSize: 13}}>
+     <div id="myGrid" className='ag-theme-alpine' style={{height: 500, width: 1365.5, fontSize: 13}}>
 
             <AgGridReact
                 //ref={gridRef}
@@ -87,9 +109,20 @@ function App () {
                 defaultColDef={defaultColDef}
                 animateRows={true}
                 rowSelection='multiple'
-                pagination={true}
+                pagination={true} 
+                paginationPageSize={tamanhoPagina}
+                rowGroupPanelShow={'always'}
+                pivotPanelShow={'always'}/>
+        </div>
 
-            />
+        <div className="sobre">
+          <h1>Sobre o Projeto</h1>
+          <p>Email: ddd@labtimw.ufg.br</p>
+        </div>
+
+        <div id="footer">
+          <p>Email: ddd@labtimw.ufg.br</p>
+          <p>telefone; 64 7888888</p>
         </div>
 
     </div>    
